af9b4f1f822ce0b6431b93233adf192599c79bcd
lusk <lucas78654321@outlook.com>
2022-07-14 14:52:24 -0300

    other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
+import Header from './components/Header';
 import NotificationButton from './components/NotificationButton';
+import SalesCard from './components/SalesCard';
 
 function App() {
   return (
     <>
-      <h1>hellow world</h1>
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
