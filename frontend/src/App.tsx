b4f55edd07515ca8b4d5addacc8cdb77f946a651
lusk <lucas78654321@outlook.com>
2022-07-14 08:49:43 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import NotificationButton from './components/NotificationButton';
+
 function App() {
   return (
-    <h1>hellow world</h1>
-  )
+    <>
+      <h1>hellow world</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
